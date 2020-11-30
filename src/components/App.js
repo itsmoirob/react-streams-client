4f6403522e43f6f8ea1cee3bfc021cf9e5da3fe7
Robbie <rob.w.sherman@gmail.com>
2020-11-30 05:58:44 +0000

    Add a header

--- src/components/App.js
+++ src/components/App.js
@@ -6,12 +6,14 @@ import StreamShow from './streams/StreamShow';
 import StreamCreate from './streams/StreamCreate';
 import StreamDelete from './streams/StreamDelete';
 import StreamEdit from './streams/StreamEdit';
+import Header from './Header';
 
 const App = () => {
   return (
-    <div>
+    <div className="ui container">
       <BrowserRouter>
         <div>
+          <Header />
           <Route path="/" exact component={StreamList} />
           <Route path="/streams/show" component={StreamShow} />
           <Route path="/streams/new" component={StreamCreate} />
